7e2b2918fb4d6bd8dbbc808ee1286d7ecc2c76a1
Fedimar Kayl Rabanzo <fedimar.rabanzo@play-asia.com>
2023-03-05 23:14:06 +0800

    initial commit

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,5 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeName } from "../store";
+
 function CarForm() {
-  return <div>CarForm</div>;
+  const dispatch = useDispatch();
+
+  const name = useSelector((state) => {
+    // Access the state
+    return state.form.name;
+  });
+
+  const handleNameChange = (event) => {
+    dispatch(changeName(event.target.value)); // Changing the state in redux store
+  };
+
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle">Add Car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CarForm;
